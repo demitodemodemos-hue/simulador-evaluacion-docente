fff3a6c61a36dc9f0f4ab344b69d3b0949de01a0
Julio Victor Tito Banegas <jtitobanegas@gmail.com>
2026-08-09 17:44:17 -0500

    preguntas y usuarios

---
+++ data/questions.js
@@ -0,0 +1,302 @@
+window.QUESTION_BANK = [
+  {
+    "id": 1,
+    "image": "assets/questions/q01.webp",
+    "correct": "B"
+  },
+  {
+    "id": 2,
+    "image": "assets/questions/q02.webp",
+    "correct": "C"
+  },
+  {
+    "id": 3,
+    "image": "assets/questions/q03.webp",
+    "correct": "C"
+  },
+  {
+    "id": 4,
+    "image": "assets/questions/q04.webp",
+    "correct": "A"
+  },
+  {
+    "id": 5,
+    "image": "assets/questions/q05.webp",
+    "correct": "A"
+  },
+  {
+    "id": 6,
+    "image": "assets/questions/q06.webp",
+    "correct": "B"
+  },
+  {
+    "id": 7,
+    "image": "assets/questions/q07.webp",
+    "correct": "A"
+  },
+  {
+    "id": 8,
+    "image": "assets/questions/q08.webp",
+    "correct": "C"
+  },
+  {
+    "id": 9,
+    "image": "assets/questions/q09.webp",
+    "correct": "B"
+  },
+  {
+    "id": 10,
+    "image": "assets/questions/q10.webp",
+    "correct": "B"
+  },
+  {
+    "id": 11,
+    "image": "assets/questions/q11.webp",
+    "correct": "A"
+  },
+  {
+    "id": 12,
+    "image": "assets/questions/q12.webp",
+    "correct": "B"
+  },
+  {
+    "id": 13,
+    "image": "assets/questions/q13.webp",
+    "correct": "C"
+  },
+  {
+    "id": 14,
+    "image": "assets/questions/q14.webp",
+    "correct": "B"
+  },
+  {
+    "id": 15,
+    "image": "assets/questions/q15.webp",
+    "correct": "A"
+  },
+  {
+    "id": 16,
+    "image": "assets/questions/q16.webp",
+    "correct": "C"
+  },
+  {
+    "id": 17,
+    "image": "assets/questions/q17.webp",
+    "correct": "A"
+  },
+  {
+    "id": 18,
+    "image": "assets/questions/q18.webp",
+    "correct": "C"
+  },
+  {
+    "id": 19,
+    "image": "assets/questions/q19.webp",
+    "correct": "A"
+  },
+  {
+    "id": 20,
+    "image": "assets/questions/q20.webp",
+    "correct": "C"
+  },
+  {
+    "id": 21,
+    "image": "assets/questions/q21.webp",
+    "correct": "A"
+  },
+  {
+    "id": 22,
+    "image": "assets/questions/q22.webp",
+    "correct": "A"
+  },
+  {
+    "id": 23,
+    "image": "assets/questions/q23.webp",
+    "correct": "C"
+  },
+  {
+    "id": 24,
+    "image": "assets/questions/q24.webp",
+    "correct": "B"
+  },
+  {
+    "id": 25,
+    "image": "assets/questions/q25.webp",
+    "correct": "C"
+  },
+  {
+    "id": 26,
+    "image": "assets/questions/q26.webp",
+    "correct": "A"
+  },
+  {
+    "id": 27,
+    "image": "assets/questions/q27.webp",
+    "correct": "B"
+  },
+  {
+    "id": 28,
+    "image": "assets/questions/q28.webp",
+    "correct": "B"
+  },
+  {
+    "id": 29,
+    "image": "assets/questions/q29.webp",
+    "correct": "C"
+  },
+  {
+    "id": 30,
+    "image": "assets/questions/q30.webp",
+    "correct": "A"
+  },
+  {
+    "id": 31,
+    "image": "assets/questions/q31.webp",
+    "correct": "A"
+  },
+  {
+    "id": 32,
+    "image": "assets/questions/q32.webp",
+    "correct": "B"
+  },
+  {
+    "id": 33,
+    "image": "assets/questions/q33.webp",
+    "correct": "A"
+  },
+  {
+    "id": 34,
+    "image": "assets/questions/q34.webp",
+    "correct": "B"
+  },
+  {
+    "id": 35,
+    "image": "assets/questions/q35.webp",
+    "correct": "B"
+  },
+  {
+    "id": 36,
+    "image": "assets/questions/q36.webp",
+    "correct": "A"
+  },
+  {
+    "id": 37,
+    "image": "assets/questions/q37.webp",
+    "correct": "B"
+  },
+  {
+    "id": 38,
+    "image": "assets/questions/q38.webp",
+    "correct": "C"
+  },
+  {
+    "id": 39,
+    "image": "assets/questions/q39.webp",
+    "correct": "A"
+  },
+  {
+    "id": 40,
+    "image": "assets/questions/q40.webp",
+    "correct": "C"
+  },
+  {
+    "id": 41,
+    "image": "assets/questions/q41.webp",
+    "correct": "C"
+  },
+  {
+    "id": 42,
+    "image": "assets/questions/q42.webp",
+    "correct": "B"
+  },
+  {
+    "id": 43,
+    "image": "assets/questions/q43.webp",
+    "correct": "B"
+  },
+  {
+    "id": 44,
+    "image": "assets/questions/q44.webp",
+    "correct": "A"
+  },
+  {
+    "id": 45,
+    "image": "assets/questions/q45.webp",
+    "correct": "A"
+  },
+  {
+    "id": 46,
+    "image": "assets/questions/q46.webp",
+    "correct": "B"
+  },
+  {
+    "id": 47,
+    "image": "assets/questions/q47.webp",
+    "correct": "C"
+  },
+  {
+    "id": 48,
+    "image": "assets/questions/q48.webp",
+    "correct": "A"
+  },
+  {
+    "id": 49,
+    "image": "assets/questions/q49.webp",
+    "correct": "B"
+  },
+  {
+    "id": 50,
+    "image": "assets/questions/q50.webp",
+    "correct": "C"
+  },
+  {
+    "id": 51,
+    "image": "assets/questions/q51.webp",
+    "correct": "A"
+  },
+  {
+    "id": 52,
+    "image": "assets/questions/q52.webp",
+    "correct": "C"
+  },
+  {
+    "id": 53,
+    "image": "assets/questions/q53.webp",
+    "correct": "B"
+  },
+  {
+    "id": 54,
+    "image": "assets/questions/q54.webp",
+    "correct": "A"
+  },
+  {
+    "id": 55,
+    "image": "assets/questions/q55.webp",
+    "correct": "B"
+  },
+  {
+    "id": 56,
+    "image": "assets/questions/q56.webp",
+    "correct": "A"
+  },
+  {
+    "id": 57,
+    "image": "assets/questions/q57.webp",
+    "correct": "C"
+  },
+  {
+    "id": 58,
+    "image": "assets/questions/q58.webp",
+    "correct": "A"
+  },
+  {
+    "id": 59,
+    "image": "assets/questions/q59.webp",
+    "correct": "C"
+  },
+  {
+    "id": 60,
+    "image": "assets/questions/q60.webp",
+    "correct": "C"
+  }
+];
