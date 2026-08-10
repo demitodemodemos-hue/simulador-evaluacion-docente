dcb9a9aff927777e14a30d1f7cbf926995bcf410
Julio Victor Tito Banegas <jtitobanegas@gmail.com>
2026-08-09 19:27:57 -0500

    key

--- data/supabase-config.js
+++ data/supabase-config.js
@@ -3,5 +3,5 @@
 // La publishable key (sb_publishable_...) está diseñada para poder usarse en el navegador.
 window.SUPABASE_CONFIG = {
   url: 'https://ipufgvictqwiaecofktr.supabase.co',
-  publishableKey: 'b_publishable_AlDD-qqHDlGNDilzxILd4w_A_jEX_pi'
+  publishableKey: 'sb_publishable_AlDD-qqHDlGNDilzxILd4w_A_jEX_pi'
 };
